cb33d5763bed34c4417288eb3783ca1d06b88994
Satyarth Mishra Sharma <mail@satyarth.me>
2022-01-31 10:41:37 +0300

    Shush annoying warning

--- preprocess/split.py
+++ preprocess/split.py
@@ -2,6 +2,7 @@ from abc import abstractmethod
 from ukb_loader import UKBDataLoader
 from numpy.random import seed, choice
 import pandas as pd
+pd.options.mode.chained_assignment = None # Shush
 
 import sys
 sys.path.append('../config')
