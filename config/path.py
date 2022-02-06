e6c54344e0e1fcd1ac5bdc966edfe3f62784793a
Satyarth Mishra Sharma <mail@satyarth.me>
2022-02-06 16:19:30 +0300

    Change path from dummy to real one

--- config/path.py
+++ config/path.py
@@ -1,4 +1,4 @@
 ukb_pfile_path = '/gpfs/gpfs0/ukb_data/plink/plink'
 ukb_loader_dir = '/gpfs/gpfs0/ukb_data/processed_data/fml'
-data_root = '/gpfs/gpfs0/ukb_data/not_fml'
+data_root = '/gpfs/gpfs0/ukb_data/fml'
 valid_ids_path = f'{data_root}/valid_ids.csv'
