417154907d684fc3227e3de15999a188caee8b59
Aleksandr Medvedev <aleksandr.medvedev@skoltech.ru>
2022-03-09 15:55:02 +0300

    Generation of FL union datasets

--- src/utils/gwas.py
+++ src/utils/gwas.py
@@ -20,6 +20,15 @@ def read_all_gwas(gwas_sources: List[str]) -> List[pandas.DataFrame]:
 
 
 def get_topk_snps(gwas: pandas.DataFrame, max_snp_count: int) -> pandas.DataFrame:
+    """Returns top {max_count_snps} SNP IDs from {gwas} GWAS results
+
+    Args:
+        gwas (pandas.DataFrame): plink 2.0 GWAS output with LOG10_P values
+        max_snp_count (int): Number of the most significant SNPs to return
+
+    Returns:
+        pandas.DataFrame: DataFrame with {max_snp_count} most significant SNPs where index is SNP ID 
+    """    
     sorted_gwas = gwas.sort_values(by='LOG10_P', axis='index', ascending=False).iloc[:max_snp_count, :]
     return sorted_gwas.drop('LOG10_P', axis='columns')
 
