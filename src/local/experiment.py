2a5997c69b32b467346551c974d81ba427c063a2
Aleksandr Medvedev <aleksandr.medvedev@skoltech.ru>
2022-04-05 11:22:54 +0300

    Lassonet working example

--- src/local/experiment.py
+++ src/local/experiment.py
@@ -14,7 +14,8 @@ import torch
 from fl.datasets.memory import load_covariates, load_phenotype, load_from_pgen, get_sample_indices
 from nn.lightning import DataModule
 from nn.train import prepare_trainer
-from nn.models import MLPRegressor
+from nn.models import MLPRegressor, LassoNetRegressor
+
 
 class LocalExperiment():
     """
@@ -34,7 +35,7 @@ class LocalExperiment():
         self.logger = logging.getLogger()
             
     def start_mlflow_run(self):
-        mlflow.set_experiment('local-mlp-standing-height')
+        mlflow.set_experiment(f'local-{self.cfg.experiment.model}-{self.cfg.phenotype.name}')
         feature_string = f'{self.cfg.experiment.snp_count} SNPs ' if self.cfg.experiment.include_genotype  \
             else '' + 'covariates' if self.cfg.experiment.include_covariates else ''
         self.run = mlflow.start_run(tags={
@@ -187,21 +188,23 @@ class NNExperiment(LocalExperiment):
     def load_data(self):
         LocalExperiment.load_data(self)
         self.data_module = DataModule(self.X_train, self.X_val, self.X_test, self.y_train, self.y_val, self.y_test, batch_size=self.cfg.model.batch_size)
-        
+    
+    def create_model(self):
+        self.model = MLPRegressor(input_size=self.X_train.shape[1],
+                                  hidden_size=self.cfg.model.hidden_size,
+                                  l1=self.cfg.model.alpha,
+                                  optim_params=self.cfg.experiment.optimizer,
+                                  scheduler_params=self.cfg.experiment.scheduler
+                                  )
+
     def train(self):
         mlflow.log_params({'model': self.cfg.model})
         mlflow.log_params({'optimizer': self.cfg.experiment.optimizer})
         mlflow.log_params({'scheduler': self.cfg.experiment.scheduler})
         
-        input_size = self.X_train.shape[1]
-        self.model = MLPRegressor(input_size=input_size,
-                                  hidden_size=self.cfg.model.hidden_size,
-                                  l1=self.cfg.model.alpha,
-                                  optim_params=self.cfg.experiment.optimizer,
-                                  scheduler_params=self.cfg.experiment.scheduler
-                                 )
+        self.create_model()
         self.trainer = prepare_trainer('models', 'logs', f'{self.cfg.experiment.model}/{self.cfg.phenotype.name}', f'run{self.run.info.run_id}', gpus=self.cfg.experiment.gpus, precision=self.cfg.model.precision,
-                                    max_epochs=self.cfg.model.max_epochs, weights_summary='full', patience=10, log_every_n_steps=5)
+                                    max_epochs=self.cfg.model.max_epochs, weights_summary='full', patience=self.cfg.model.patience, log_every_n_steps=5)
         
         print("Fitting")
         self.trainer.fit(self.model, self.data_module)
@@ -239,6 +242,62 @@ class NNExperiment(LocalExperiment):
         mlflow.log_metric('val_r2', r2_val)
         print(f"Test r2: {r2_test}")
         mlflow.log_metric('test_r2', r2_test)
+
+
+class LassoNetExperiment(NNExperiment):
+
+    def create_model(self):
+        self.model = LassoNetRegressor(
+            input_size=self.X_train.shape[1],
+            hidden_size=self.cfg.model.hidden_size,
+            optim_params=self.cfg.experiment.optimizer,
+            scheduler_params=self.cfg.experiment.scheduler,
+            alpha_start=self.cfg.model.alpha_start,
+            alpha_end=self.cfg.model.alpha_end,
+            init_limit=self.cfg.model.init_limit
+        )
+
+    def load_best_model(self):
+        self.model = LassoNetRegressor.load_from_checkpoint(
+            self.trainer.checkpoint_callback.best_model_path,
+            input_size=self.X_train.shape[1],
+            hidden_size=self.cfg.model.hidden_size,
+            optim_params=self.cfg.experiment.optimizer,
+            scheduler_params=self.cfg.experiment.scheduler,
+            alpha_start=self.cfg.model.alpha_start,
+            alpha_end=self.cfg.model.alpha_end,
+            init_limit=self.cfg.model.init_limit
+        )
+
+    def eval_and_log(self):
+        self.model.eval()
+        train_preds, val_preds, test_preds = self.trainer.predict(self.model, self.data_module)
+        
+        train_preds = torch.cat(train_preds).squeeze().cpu().numpy()
+        val_preds = torch.cat(val_preds).squeeze().cpu().numpy()
+        test_preds = torch.cat(test_preds).squeeze().cpu().numpy()
+        
+        print(train_preds.shape, val_preds.shape, test_preds.shape)
+        
+        best_train_r2 = -1e+3
+        best_val_r2 = -1e+3
+        best_test_r2 = -1e+3
+        best_col = None
+        for col in range(val_preds.shape[1]):
+            r2_val = r2_score(self.y_val, val_preds[:, col])
+            if r2_val > best_val_r2:
+                best_col = col
+                best_val_r2 = r2_val
+                best_train_r2 = r2_score(self.y_train, train_preds[:, col])
+                best_test_r2 = r2_score(self.y_test, test_preds[:, col])
+
+        print(f'Best alpha: {self.model.alphas[best_col]:.6f}')
+        print(f"Train r2: {best_train_r2:.4f}")
+        mlflow.log_metric('train_r2', best_train_r2)
+        print(f"Val r2: {best_val_r2:.4f}")
+        mlflow.log_metric('val_r2', best_val_r2)
+        print(f"Test r2: {best_test_r2:.4f}")
+        mlflow.log_metric('test_r2', best_test_r2)
         
         
 # todo: model configs from hydra?            
@@ -252,11 +311,12 @@ xgb_kwargs_dict = {
 experiment_dict = {
     #'lasso': simple_estimator_decorator(LassoCV),
     #'xgboost': xgboost_decorator(xgb_kwargs_dict),
-    'mlp': NNExperiment
+    'mlp': NNExperiment,
+    'lassonet': LassoNetExperiment
 }
 
             
-@hydra.main(config_path='configs', config_name='default')
+@hydra.main(config_path='configs', config_name='lassonet')
 def local_experiment(cfg: DictConfig):
     assert cfg.experiment.model in experiment_dict.keys()
     experiment = experiment_dict[cfg.experiment.model](cfg)
