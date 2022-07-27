b79a1047bd4bda4fd8155f46d14b7ef422d000c2
Satyarth Mishra Sharma <mail@satyarth.me>
2022-07-27 11:24:34 +0300

    Working test script

--- src/local/test.py
+++ src/local/test.py
@@ -1,49 +1,55 @@
 import hydra
-from omegaconf import OmegaConf
+from omegaconf import DictConfig
+
 import mlflow
 import numpy as np
 from pytorch_lightning import Trainer
 import torch
-import numpy as np
 from sklearn.metrics import r2_score
+
 from local.experiment import LassoNetExperiment
 
-# hydra.core.global_hydra.GlobalHydra().clear()
-hydra.initialize(config_path="configs")
-cfg = hydra.compose(config_name="test", overrides=['+model=lassonet'])
+@hydra.main(config_path='configs', config_name='test')
+def test(cfg: DictConfig):
+    print(cfg)
+    
+    experiment = LassoNetExperiment(cfg)
+    experiment.load_sample_indices()
+    experiment.load_data()
+    
+    print("Loading train experiment")
+    train_experiment = mlflow.get_experiment_by_name('ethnic-split-all-snps-train')
+    runs = mlflow.list_run_infos(train_experiment.experiment_id)
+    df = mlflow.search_runs(experiment_ids=[train_experiment.experiment_id])
 
-print(OmegaConf.to_yaml(cfg))
+    assert(sum(df['tags.node_index'] == str(cfg.train_node_index)) == 1)
+    mlflow_run_id = df.loc[(df['tags.node_index'] == str(cfg.train_node_index)) and (df['tags.snp_count'] == '200000'), 'run_id'].values[0]
+    run = mlflow.get_run(mlflow_run_id)
+    loaded = mlflow.pytorch.load_model(f"runs:/{mlflow_run_id}/lassonet-model")
+    loaded.eval()
+    
+    print("Predicting")
+    
+    trainer = Trainer(gpus=0)
+    train_preds, val_preds, test_preds = trainer.predict(loaded, experiment.data_module)
 
-experiment = LassoNetExperiment(cfg)
-experiment.load_sample_indices()
-experiment.load_data()
+    best_col = int(run.data.metrics['best_alpha_index'])
+    best_val_r2 = r2_score(experiment.y_val, torch.cat(val_preds)[:, best_col].numpy())
+    best_train_r2 = r2_score(experiment.y_train, torch.cat(train_preds)[:, best_col].numpy())
+    best_test_r2 = r2_score(experiment.y_test, torch.cat(test_preds)[:, best_col].numpy())
 
-train_node_index=1
-loaded = mlflow.pytorch.load_model(f'models:/lassonet_region_split_node_{experiment.cfg.train_node_index}/1')
-loaded.eval()
+    print("Logging")
+    
+    mlflow.set_experiment(experiment.cfg.experiment.name)
+    with mlflow.start_run(tags={'test_node_index': str(experiment.cfg.node_index),
+                                'train_node_index': str(experiment.cfg.train_node_index)}):
+        print(f"Train r2: {best_train_r2}")
+        mlflow.log_metric('train_r2', best_train_r2)
+        print(f"Val r2: {best_val_r2}")
+        mlflow.log_metric('val_r2', best_val_r2)
+        print(f"Test r2: {best_test_r2}")
+        mlflow.log_metric('test_r2', best_test_r2)
 
-trainer = Trainer(gpus=0)
-train_preds, val_preds, test_preds = trainer.predict(loaded,
-                                                     [experiment.data_module.train_dataloader(),
-                                                      experiment.data_module.val_dataloader(),
-                                                      experiment.data_module.test_dataloader()])
-
-train_preds = torch.cat(train_preds).numpy()
-val_preds = torch.cat(val_preds).numpy()
-test_preds = torch.cat(test_preds).numpy()
-
-r2_val_list = [r2_score(experiment.y_val, val_preds[:, col]) for col in range(val_preds.shape[1])]
-best_col = np.argmax(r2_val_list)
-best_val_r2 = np.amax(r2_val_list)
-best_train_r2 = r2_score(experiment.y_train, train_preds[:, best_col])
-best_test_r2 = r2_score(experiment.y_test, test_preds[:, best_col])
-
-mlflow.set_experiment(experiment.cfg.experiment.name)
-with mlflow.start_run(tags={'test_node_index': str(experiment.cfg.node_index),
-                            'train_node_index': str(experiment.cfg.train_node_index)}):
-    print(f"Train r2: {best_train_r2}")
-    mlflow.log_metric('train_r2', best_train_r2)
-    print(f"Val r2: {best_val_r2}")
-    mlflow.log_metric('val_r2', best_val_r2)
-    print(f"Test r2: {best_test_r2}")
-    mlflow.log_metric('test_r2', best_test_r2)
+if __name__ == '__main__':
+    test()
+    
