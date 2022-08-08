206140efef998b2c9c8498c9ae93b356f8ebf721
dkolobok <dmitry.s.kolobkov@gmail.com>
2022-08-08 14:53:18 +0100

    use mlflow steps

--- src/utils/mlflow.py
+++ src/utils/mlflow.py
@@ -39,33 +39,16 @@ def assemble_metric_history(client, experiment_name, selected_tags, per_epoch_me
     runs_df['epochs_in_round'] = [eval(i).get('epochs_in_round') for i in runs_df['params.scheduler']]
     runs_df['run_name'] = runs_df['total_rounds'].astype(str) + ', ' + runs_df['params.name']
     logging.info('extracting metrics')
-    metrics_data = [[[i.value for i in client.get_metric_history(run_id=run_id, key=key)] for key in all_metrics] for run_id in runs_df['run_id'].to_list()]
+    metrics_data = [[{i.step: i.value for i in client.get_metric_history(run_id=run_id, key=key)} for key in all_metrics] for run_id in runs_df['run_id'].to_list()]
     logging.info('done!')
     df = pd.DataFrame(metrics_data, columns=pd.Index(all_metrics, name='metric_name'),
                       index=runs_df.set_index(list(set(['run_id', 'id', 'run_name', 'total_rounds', 'epochs_in_round',
                                                         'params.name'] + selected_tags))).index)
     ser = df.stack()
-
-    def insert_nan(x, block_length, desired_length, node_name, current_metric):
-        """ Inserts nans in x for the server node. E.g. insert_nan([1, 2, 3], 3, 7) gives [1, nan, nan, 2, nan, nan, 3] """
-        if ((current_metric in per_epoch_metrics) and (node_name != 'server')) or (len(x) == 0):
-            # no need to insert nans
-            return x
-        y = np.full((len(x) - 1) * (block_length + 1) + 1, np.nan)
-        y[::(block_length + 1)] = x
-        assert len(y) == desired_length
-        return y
-
     total_epochs = ser.apply(len).groupby('id').max()
     total_epochs.name = 'total_epochs'
     df = ser.to_frame().join(total_epochs).set_index('total_epochs', append=True)
-    df['metric_value'] = [insert_nan(df[0].iloc[i], block_length=df.index.get_level_values('epochs_in_round')[i],
-                                     desired_length=df.index.get_level_values('total_epochs')[i],
-                                     node_name=df.index.get_level_values('params.name')[i],
-                                     current_metric=df.index.get_level_values('metric_name')[i]) for i in range(len(df))]
-
-
-    df = pd.DataFrame(df['metric_value'].tolist(), index=df.index)
+    df = pd.DataFrame([pd.Series(d) for d in df[0].values], index = df.index).sort_index(axis=1)
     df.columns.name = 'epoch'
     df = df.stack().reset_index().rename(columns={0: 'metric_value'})
     return df[['params.name', 'total_rounds', 'epoch', 'metric_name', 'metric_value']]
