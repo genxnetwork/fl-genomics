dfc59f383b19b0482e772bf3259e738409d9dd34
Egor Kosaretskiy <megametagross@outlook.de>
2022-08-22 16:21:07 +0300

    [BAR-155] Added simple docstrings

--- src/preprocess/pruning.py
+++ src/preprocess/pruning.py
@@ -10,6 +10,10 @@ class Pruning(object):
 
     @staticmethod
     def prune(NODE: str, window_size: int, step: int, threshold: float):
+        """
+        Creates lists of variants which LD is above
+        threshold in NODE.preprune.out and below threshold in NODE.preprune.in
+        """
         if not 0 <= threshold <= 1:
             raise ValueError(f'{threshold} is an invalid threshold value! It should be set between 0 and 1!')
 
@@ -33,6 +37,9 @@ class Pruning(object):
 
     @staticmethod
     def merge(node: str = 'ALL'):
+        """
+        Merges all nodes in TG_SUPERPOP_DICT NODE.preprune.in lists into one.
+        """
         nodes = set(TG_SUPERPOP_DICT.values())
         nodes = (path.join(SPLIT_GENO_DIR, node + '_filtered.prune.in') for node in nodes)
         system(f"cat {' '.join(nodes)} | sort | uniq > {path.join(SPLIT_GENO_DIR, f'{node}.prune.in')}")
@@ -40,6 +47,9 @@ class Pruning(object):
 
     @staticmethod
     def remove_variants(NODE: str, SNPS: str):
+        """
+        Removes variants from NODE pfiles that are in list SNPS.prune.in
+        """
         run_plink(
             args_list=[
                 '--pfile', path.join(SPLIT_GENO_DIR, NODE),
