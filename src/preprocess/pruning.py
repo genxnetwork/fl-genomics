783af9aba791d1f553eb6ad7669e0e2698f32643
Egor Kosaretskiy <52824960+Jahysama@users.noreply.github.com>
2022-08-29 11:51:17 +0300

    [BAR-155] Fixed pruning

--- src/preprocess/pruning.py
+++ src/preprocess/pruning.py
@@ -1,5 +1,6 @@
 from os import path
 from os import system
+from pathlib import Path
 
 from utils.plink import run_plink
 
@@ -14,18 +15,8 @@ class Pruning(object):
         Creates lists of variants which LD is above
         threshold in NODE.preprune.out and below threshold in NODE.preprune.in
         """
-        if not 0 <= threshold <= 1:
+        if not 0 <= threshold < 1:
             raise ValueError(f'{threshold} is an invalid threshold value! It should be set between 0 and 1!')
-
-        run_plink(
-            args_list=[
-                '--pfile', path.join(SPLIT_GENO_DIR, f'{NODE}'),
-                '--out', path.join(SPLIT_GENO_DIR, f'{NODE}'),
-                '--rm-dup',
-                '--set-missing-var-ids', '@:#',
-                '--make-pgen'
-            ]
-        )
 
         run_plink(
             args_list=[
@@ -36,20 +27,31 @@ class Pruning(object):
         )
 
     @staticmethod
-    def merge(node: str = 'ALL'):
+    def merge(node: str = 'ALL', nodes_list: list = list(set(TG_SUPERPOP_DICT.values()))):
         """
         Merges all nodes in TG_SUPERPOP_DICT NODE.preprune.in lists into one.
         """
-        nodes = set(TG_SUPERPOP_DICT.values())
-        nodes = (path.join(SPLIT_GENO_DIR, node + '_filtered.prune.in') for node in nodes)
-        system(f"cat {' '.join(nodes)} | sort | uniq > {path.join(SPLIT_GENO_DIR, f'{node}.prune.in')}")
+
+        nodes_list = (path.join(SPLIT_GENO_DIR, node + '_filtered.prune.in') for node in nodes_list)
+        system(f"cat {' '.join(nodes_list)} | sort | uniq > {path.join(SPLIT_GENO_DIR, f'{node}.prune.in')}")
 
 
     @staticmethod
-    def remove_variants(NODE: str, SNPS: str):
+    def remove_variants(NODE: str, SNPS: str, no_pruning=True):
         """
         Removes variants from NODE pfiles that are in list SNPS.prune.in
         """
+        if no_pruning:
+            Path(path.join(SPLIT_GENO_DIR, 'empty.in')).touch()
+            run_plink(
+                args_list=[
+                    '--pfile', path.join(SPLIT_GENO_DIR, NODE),
+                    '--out', path.join(SPLIT_GENO_DIR, f'{NODE}.pruned'),
+                    '--exclude', path.join(SPLIT_GENO_DIR, f'empty.in'),
+                    '--make-pgen'
+                ]
+            )
+
         run_plink(
             args_list=[
                 '--pfile', path.join(SPLIT_GENO_DIR, NODE),
@@ -58,7 +60,3 @@ class Pruning(object):
                 '--make-pgen'
             ]
         )
-
-
-
-
