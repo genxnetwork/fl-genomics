fa27ab17bff844ba42e48ba862f417773ffcc67a
Egor Kosaretskiy <megametagross@outlook.de>
2022-09-21 23:29:05 +0300

    [BAR-189] Major refractoring

--- src/analysis/sum_stat.py
+++ src/analysis/sum_stat.py
@@ -32,14 +32,15 @@ class SummaryStat(object):
         return False, False
 
     def __get_ss_info(self, header: list, tag: str):
-        pos_i, pos = self.__search("^[Pp][Oo][Ss]*", header)
+        pos_i, pos = self.__search("[Pp][Oo][Ss]*", header)
         id_i, id = self.__search("^[Ii][Dd]*", header)
-        p_i, p = self.__search("^[Pp].*val*|^[Ll][Oo][Gg].*[Pp]*|^[Pp]$", header, pvalue=True)
+        p_i, p = self.__search("[Pp].*val*|[Ll][Oo][Gg].*[Pp]*|^[Pp]$", header, pvalue=True)
+        chr_i, chr = self.__search("[Cc][Hh][Rr]*", header)
 
         ss_info = {'pos': pos, 'id': id, 'p': p,
                    'pos_i': pos_i, 'id_i': id_i, 'p_i': p_i,
-                   'tag': tag}
-        cols = [pos, id]
+                   'tag': tag, 'chr': chr, 'chr_i': chr_i}
+        cols = [pos, id, chr]
         for item in cols:
             if not item:
                 cols.remove(item)
@@ -52,42 +53,41 @@ class SummaryStat(object):
         return ss_info
 
     def __get_dataframe(self, ss: str, ss_info: dict, norm: bool):
-        df = pd.read_table(ss + '.filtered', skipinitialspace=True, usecols=ss_info[ss]['cols'])
-        df_filtered = \
-            df.drop_duplicates(subset=[ss_info[ss]['pos']]) \
-                .sort_values(by=[ss_info[ss]['pos']]) \
-                .reset_index(drop=True)
 
-        for pvalue in ss_info[ss]['p']:
-            if df_filtered[pvalue].max() > 1:
-                df_filtered[pvalue] = np.log10(df_filtered[pvalue])
-            if df_filtered[pvalue].min() < 0:
-                df_filtered[pvalue] = df_filtered[pvalue].apply(lambda x: x * -1)
+        df = pd.read_table(ss + '.filtered', skipinitialspace=True,
+                           usecols=ss_info[ss]['cols'],
+                           dtype={ss_info[ss]['chr']: 'string'}
+                           )
+        keep_same = {ss_info[ss]['pos'], ss_info[ss]['chr']}
+        df.columns = ['{}{}'.format(c, '' if c in keep_same else '_' + ss_info[ss]['tag']) for c in df.columns]
+        new_p = [s + f"_{ss_info[ss]['tag']}" for s in ss_info[ss]['p']]
+        for pvalue in new_p:
+            if df[pvalue].max() <= 1:
+                logging.info(f'Making log10 {pvalue} in {ss} ...')
+                df[pvalue] = np.log10(df[pvalue])
+            if df[pvalue].min() < 0:
+                logging.info(f'Making -log10 {pvalue} in {ss} ...')
+                df[pvalue] = df[pvalue].apply(lambda x: x * -1)
             if norm:
                 logging.info(f'Performing min max normalisation for {ss}...')
-                df_filtered[pvalue] = (df_filtered[pvalue] - df_filtered[pvalue].min()) / \
-                             (df_filtered[pvalue].max() - df_filtered[pvalue].min())
+                df[pvalue] = (df[pvalue] - df[pvalue].min()) / \
+                             (df[pvalue].max() - df[pvalue].min())
 
-        keep_same = {ss_info[ss]['pos'], ss_info[ss]['id']}
-        df_filtered.columns = ['{}{}'.format(c, '' if c in keep_same else '_'+ss_info[ss]['tag']) for c in df_filtered.columns]
+        df_filtered = \
+            df.melt(id_vars=[ss_info[ss]['chr'], ss_info[ss]['pos']], value_vars=new_p)\
+                .reset_index(drop=True)
         return df_filtered
 
     def __merge_data(self, df_dict: dict, ss_info: dict):
-        first_time = True
-        for ss in df_dict:
-            if first_time:
-                previous_ss = df_dict[ss].rename(columns={ss_info[ss]['pos']: 'pos', ss_info[ss]['id']: 'id'})
-                first_time = False
-                continue
-            df = df_dict[ss].rename(columns={ss_info[ss]['pos']: 'pos', ss_info[ss]['id']: 'id'})
-            previous_ss = pd.merge(df, previous_ss, how="inner", on=["pos", "id"])
-
-        return previous_ss
+        df_list = list(df_dict.values())
+        for index, _ in enumerate(df_list):
+            df_list[index].columns = ['chr', 'pos', 'var', 'pvalue']
+        return pd.concat(df_list, axis=0, ignore_index=True)
 
     def prepare_data(self, *ss_and_tag: tuple[str], working_dir: str, to_file='ss.tsv', norm=True) -> pandas.DataFrame:
         '''
         Returns aggregated and intersected summary statistics dataframe.
-        All summary statistics files must contain position column and at least one of them should have id column.
+        All summary statistics files must contain position and chromosome column.
         All necessary columns will be detected automatically with regex.
 
                 Parameters:
@@ -97,8 +97,9 @@ class SummaryStat(object):
                         norm (bool): Perform min max normalisation for each p-value column separately
 
                 Returns:
-                        merged_df (pandas.DataFrame): Dataframe with position, id and all -log10(p-values) columns from all input files
+                        merged_df (pandas.DataFrame): Dataframe with columns: chromosome, position, pvalue tag, -log10(pvalue)
         '''
+
         ss_info = {}
         for i, ss_tag in enumerate(ss_and_tag):
             ss, tag = ss_tag
@@ -110,24 +111,26 @@ class SummaryStat(object):
 
             logging.info(f'Preparing list of positions for {ss}...')
             pos_list_dist = os.path.join(working_dir, f'pos{i}.list')
-            subprocess.call("awk -F '\t' '{ print $%s }' %s | sort | uniq > %s"
-                            % (ss_info[ss]['pos_i'], ss, pos_list_dist),
+            subprocess.call("awk -F '\t' '{ print $%s, $%s }' %s | tail -n +2 | sort | uniq > %s"
+                            % (ss_info[ss]['chr_i'], ss_info[ss]['pos_i'], ss, pos_list_dist),
                             shell=True)
 
         logging.info(f'Making lists of positions intersection...')
         intersection_list_dist = os.path.join(working_dir, f'intersection.list')
-        subprocess.call("cat pos* | sort | uniq -c | awk '{if($1==%s){print $2}}' > %s"
-                        % (len(ss_info), intersection_list_dist),
+        subprocess.call("""cat %s | tail -n +2 | sort | uniq -c | awk -F ' ' '{if($1==%s){print $2":"$3}}' > %s"""
+                        % (os.path.join(working_dir, 'pos*'), len(ss_info), intersection_list_dist),
                         shell=True)
 
         df_dict = {}
-        found_ids = False
-        no_ids_list = []
+
         for i, ss_tag in enumerate(ss_and_tag):
             ss, tag = ss_tag
             logging.info(f'Filtering rows based on intersection list for {ss}...')
-            subprocess.call("awk 'NR==FNR{a[$1]; next} FNR==1 || $%s in a' %s %s > %s"
-                            % (ss_info[ss]['pos_i'],intersection_list_dist, ss, ss + '.filtered'),
+            subprocess.call(""" awk '{print $0"\t"$1":"$2;next}' %s > %s"""
+                            % (ss, ss + '.mod'),
+                            shell=True)
+            subprocess.call("awk 'NR==FNR{a[$1]; next} FNR==1 || $NF in a' %s %s > %s"
+                            % (intersection_list_dist, ss + '.mod', ss + '.filtered'),
                             shell=True)
 
             logging.info(f'Creating pandas dataframe for {ss}...')
@@ -135,66 +138,54 @@ class SummaryStat(object):
 
             df_dict[ss] = df
 
-            if ss_info[ss]['id'] and not found_ids:
-                id = df[ss_info[ss]['id']]
-                found_ids = True
-            elif not ss_info[ss]['id']:
-                no_ids_list.append(ss)
-
-        if no_ids_list:
-            for ss in no_ids_list:
-                df_dict[ss]['id'] = id
-
         logging.info(f'Merging all dataframes into one...')
         df_combined = self.__merge_data(df_dict, ss_info)
         if to_file:
             logging.info(f'Writing resulting dataframe to {to_file}...')
             df_combined.to_csv(to_file, sep="\t", index=False)
-        return df_combined
+        return df_combined.dropna()
+
 
     def plot(self, df: pandas.DataFrame,
              super_pop_list=['AFR', 'AMR', 'EAS', 'EUR', 'SAS'],
              top_snps_list=[10, 20, 30, 40, 50, 100, 1000, 10000],
-             to_folder='~'):
+             to_folder='/home/genxadmin'):
+
         '''
-        Returns aggregated and intersected summary statistics dataframe.
-        All summary statistics files must contain position column and at least one of them should have id column.
-        All necessary columns will be detected automatically with regex.
+        Creates simple summary statistics top N snps plot
 
                 Parameters:
-                        ss_and_tag (tuple): Any number of tuples with format: (path_to_summary_stat.tsv, ancestry_tag)
-                        working_dir (str): Path to a dir to store all lists into
-                        to_file (str or None): Path to output dataframe, if None won't create one
-                        norm (bool): Perform min max normalisation for each p-value column separately
-
-                Returns:
-                        merged_df (pandas.DataFrame): Dataframe with position, id and all -log10(p-values) columns from all input files
+                        df (pandas.DataFrame): Dataframe, output of prepare_data
+                        super_pop_list (list[str]): List of super population tags, used to group pvalues
+                        top_snps_list (list[int]): List of values for x-axis of the plot, amount of top snps for each point on the plot
+                        to_folder (str): path to folder to store plots pngs into
         '''
 
+        groups = df.groupby('var')
         grouping_dict = {k: [] for k in super_pop_list}
-        for col in df.columns:
+        for name, group in groups:
             for pop in super_pop_list:
-                if pop in col:
-                    grouping_dict[pop].append(col)
+                if pop in name:
+                    grouping_dict[pop].append(name)
 
         for group in grouping_dict:
             if len(grouping_dict[group]) < 2:
                 continue
             plot_dict = {}
             for value in top_snps_list:
-                df_list = []
-                for col in grouping_dict[group]:
-                    df_g = df[[col, 'id', 'pos']]
-                    df_g = df_g.sort_values([col], ascending=False).head(value)
-                    df_list.append(df_g)
+                g_list = []
+                for name in grouping_dict[group]:
+                    g = groups.get_group(name)\
+                        .sort_values(['pvalue'], ascending=False).head(value)
+                    g_list.append(g)
 
                 first_time = True
-                for data in df_list:
+                for data in g_list:
                     if first_time:
                         previous = data
                         first_time = False
                         continue
-                    previous = pd.merge(data, previous, how="inner", on=["pos", "id"])
+                    previous = pd.merge(data, previous, how="inner", on=["pos", "chr"])
                 plot_dict[str(value)] = len(previous)
 
             keys = list(plot_dict.keys())
@@ -204,4 +195,3 @@ class SummaryStat(object):
                 set(title=f'{group}', xlabel='Number of top snps', ylabel='Number of common snps')
             if to_folder:
                 plt.savefig(os.path.join(to_folder, f'{group}.png'))
-                
