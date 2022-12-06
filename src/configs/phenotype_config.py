8825c79bd3a636c453f52dde4f8caacc3bca2dcb
Satyarth Mishra Sharma <mail@satyarth.me>
2022-12-06 23:23:01 +0300

    Binary phenotypes in phenotype_config.py

--- src/configs/phenotype_config.py
+++ src/configs/phenotype_config.py
@@ -7,38 +7,48 @@ MEAN_PHENO_DICT = {'standing_height': 170.0,
                    'ebmd': -0.33}
 
 QUANTITATIVE_PHENO_LIST = [
-    'standing_height',
-    'platelet_count',
-    'platelet_volume',
-    'body_mass_index',
-    'basal_metabolic_rate',
-    'hls_reticulocyte_count',
-    'forced_vital_capacity',
-    'erythrocyte_count',
-    'reticulocyte_count',
-    'mean_sphered_cell_volume',
-    'triglycerides',
-    'hdl_cholesterol',
-    'vitamin_d',
-    'alanine_aminotransferase']
+        'standing_height',
+        'platelet_volume',
+        'erythrocyte_count',
+        'triglycerides',
+        'hdl_cholesterol',
+        'vitamin_d',
+        'creatinine',
+        'alkaline_phosphatase',
+        'cystatin_c',
+        'gamma_glutamyltransferase',
+        'ebmd',
+        'apolipoprotein_a',
+        'shbg']
+
+BINARY_PHENO_LIST = [
+        'asthma',
+        'hypothyroidism', 
+        'hypertension',
+        'diabetes',
+        'psoriasis',
+        'rhinitis',
+        'osteoarthritis',
+        'depression',
+        'gastric_reflux',
+        'angina',
+        'migraine'
+        ]
 
 PHENO_TYPE_DICT = {'ancestry': 'discrete',
-                   'simulated': 'continuous',
-                   'asthma': 'binary'}
+                   'simulated': 'continuous'}
 
 TYPE_LOSS_DICT = {'continuous': mse_loss,
                   'discrete': cross_entropy,
                   'binary': binary_cross_entropy}
 
 PHENO_NUMPY_DICT = {'standing_height': np.float32,
-                    'ebmd': np.float32,
-                    'ancestry': np.ndarray,
-                    'asthma': np.float32,
-                    'platelet_volume': np.float32}
-
+                        'ancestry': np.ndarray}
 
 PHENO_TYPE_DICT.update({phenotype: 'continuous' for phenotype in QUANTITATIVE_PHENO_LIST})
+PHENO_TYPE_DICT.update({phenotype: 'binary' for phenotype in BINARY_PHENO_LIST})
 PHENO_NUMPY_DICT.update({phenotype: np.float32 for phenotype in QUANTITATIVE_PHENO_LIST})
+PHENO_NUMPY_DICT.update({phenotype: np.float32 for phenotype in BINARY_PHENO_LIST})
 
 def get_accuracy(y_true: np.array, y_pred: torch.tensor) -> float:
     """ Takes predictions, gets most probable class and compares with y to get accuracy """
