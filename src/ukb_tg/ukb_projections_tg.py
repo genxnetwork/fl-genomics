3692fd18fd679eb5b1b0be7605c6bfe4cba1cf15
dkolobok <dmitry.s.kolobkov@gmail.com>
2023-03-07 11:01:04 +0000

    provide num_pcs as a parameter

--- src/ukb_tg/ukb_projections_tg.py
+++ src/ukb_tg/ukb_projections_tg.py
@@ -16,6 +16,79 @@ from local.tg_simple_trainer import SimpleTrainer
 from preprocess.splitter_tg import SplitTG
 from utils.plink import run_plink
 
+TG_EXT_DIR = os.path.join(TG_DATA_ROOT, 'external')
+TG_UKB_DIR = os.path.join(TG_EXT_DIR, 'ukb')
+
+
+class UkbProjectionsTg(object):
+    def __init__(self, num_pcs=20):
+        logger.info(f'Initializing class with {num_pcs} PCs')
+        self.num_pcs = num_pcs
+        self.tg_pca_prefix = os.path.join(TG_UKB_DIR, 'tg_pca')
+        self.tg_pca_prefix_pcs = f'{self.tg_pca_prefix}_{num_pcs}pcs'
+
+    def main(self):
+        # 1. Take UKB variants file and leave only the rsid column
+        ukb_variants_fn = os.path.join(TG_UKB_DIR, 'ukb_filtered.bim')
+        ukb_rsids_fn = os.path.join(TG_UKB_DIR, 'ukb_rsids.txt')
+        os.system("awk '{print $2}' " + ukb_variants_fn + " > " + ukb_rsids_fn)
+
+        # 2. Extract intersection variants
+        logger.info(f'Filtering by samples present both in UKB and TG')
+        all_filtered_fn = os.path.join(SPLIT_GENO_DIR, 'ALL_filtered')
+        tg_filt_prefix = os.path.join(TG_UKB_DIR, 'tg_filt')
+        run_plink(
+            args_list=[
+                '--pfile', all_filtered_fn,
+                '--extract', ukb_rsids_fn,
+                '--make-pgen',
+                '--out', tg_filt_prefix,
+            ]
+        )
+
+        # 3. Conduct PCA on filtered TG data
+        logger.info(f'Running centralised PCA on all TG samples for variants present in UKB and TG')
+        run_plink(
+            args_list=[
+                '--pfile', tg_filt_prefix,
+                '--freq', 'counts',
+                '--out', self.tg_pca_prefix,
+                '--pca', 'allele-wts', self.num_pcs
+            ]
+        )
+
+        # 4. Get TG data projections onto TG's PC space (1 / sqrt(.eigenval) scaling)
+        logger.info(f"Getting TG data projections onto TG's PC space (1 / sqrt(.eigenval) scaling)")
+        run_plink(
+            args_list=[
+                '--pfile', all_filtered_fn,
+                '--read-freq', self.tg_pca_prefix + '.acount',
+                '--score', self.tg_pca_prefix + '.eigenvec.allele',
+                '2', '5', 'header-read', 'no-mean-imputation', 'variance-standardize', '--score-col-nums', f'6-{5 + self.num_pcs}',
+                '--out', self.tg_pca_prefix_pcs,
+                '--set-missing-var-ids', '@:#'
+            ]
+        )
+
+        logger.info(f'Done!')
+
+        # 5. Write TG phenotypes
+        ancestry_df = SplitTG().get_target()
+        relevant_ids = ancestry_df['IID'].isin(pd.read_csv(all_filtered_fn + '.psam', sep='\t')['#IID'])
+        ancestry_df.loc[relevant_ids, ['IID', 'ancestry']].to_csv(self.tg_pca_prefix + '.tsv', sep='\t', index=False)
+
+        # 6. Train a model
+        x = pd.read_csv(self.tg_pca_prefix_pcs + '.sscore', sep='\t').rename(columns={'#IID': 'IID'}).set_index('IID').filter(
+            like='_AVG')
+        y = pd.read_csv(self.tg_pca_prefix + '.tsv', sep='\t').set_index('IID').reindex(x.index)['ancestry']
+        _, y = np.unique(y, return_inverse=True)
+        # # CV is commented out because it's needed only to check performance
+        # SimpleTrainer(nclass=len(np.unique(y)), nfeat=x.shape[1], epochs=10000, lr=0.1).run_cv(x=x.values, y=y, K=10)
+        SimpleTrainer(nclass=len(np.unique(y)), nfeat=x.shape[1], epochs=10000, lr=0.1).train_and_save(x=x.values, y=y,
+                                                                                                       out_fn=f'{self.tg_pca_prefix_pcs}.pkl')
+        logger.info(f'Done!')
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         stream=sys.stdout,
@@ -23,67 +96,7 @@ if __name__ == '__main__':
                         datefmt='%Y-%m-%d %H:%M:%S'
                         )
     logger = logging.getLogger()
+    for num_pcs in [10, 20, 30, 40]:
+        UkbProjectionsTg(num_pcs=num_pcs).main()
 
-    TG_EXT_DIR = os.path.join(TG_DATA_ROOT, 'external')
-    TG_UKB_DIR = os.path.join(TG_EXT_DIR, 'ukb')
-
-    # 1. Take UKB variants file and leave only the rsid column
-    ukb_variants_fn = os.path.join(TG_UKB_DIR, 'ukb_filtered.bim')
-    ukb_rsids_fn = os.path.join(TG_UKB_DIR, 'ukb_rsids.txt')
-    os.system("awk '{print $2}' " + ukb_variants_fn + " > " + ukb_rsids_fn)
-
-    # 2. Extract intersection variants
-    logger.info(f'Filtering by samples present both in UKB and TG')
-    all_filtered_fn = os.path.join(SPLIT_GENO_DIR, 'ALL_filtered')
-    tg_filt_prefix = os.path.join(TG_UKB_DIR, 'tg_filt')
-    run_plink(
-        args_list=[
-            '--pfile', all_filtered_fn,
-            '--extract', ukb_rsids_fn,
-            '--make-pgen',
-            '--out', tg_filt_prefix,
-        ]
-    )
-
-    # 3. Conduct PCA on filtered TG data
-    logger.info(f'Running centralised PCA on all TG samples for variants present in UKB and TG')
-    tg_pca_prefix = os.path.join(TG_UKB_DIR, 'tg_pca')
-    run_plink(
-        args_list=[
-            '--pfile', tg_filt_prefix,
-            '--freq', 'counts',
-            '--out', tg_pca_prefix,
-            '--pca', 'allele-wts', '10'
-        ]
-    )
-
-    # 4. Get TG data projections onto TG's PC space (1 / sqrt(.eigenval) scaling)
-    logger.info(f"Getting TG data projections onto TG's PC space (1 / sqrt(.eigenval) scaling)")
-    run_plink(
-        args_list=[
-            '--pfile', all_filtered_fn,
-            '--read-freq', tg_pca_prefix + '.acount',
-            '--score', tg_pca_prefix + '.eigenvec.allele',
-            '2', '5', 'header-read', 'no-mean-imputation', 'variance-standardize', '--score-col-nums', '6-15',
-            '--out', tg_pca_prefix,
-            '--set-missing-var-ids', '@:#'
-        ]
-    )
-
-    logger.info(f'Done!')
-
-    # 5. Write TG phenotypes
-    ancestry_df = SplitTG().get_target()
-    relevant_ids = ancestry_df['IID'].isin(pd.read_csv(all_filtered_fn + '.psam', sep='\t')['#IID'])
-    ancestry_df.loc[relevant_ids, ['IID', 'ancestry']].to_csv(tg_pca_prefix + '.tsv', sep='\t', index=False)
-
-    # 6. Train a model
-    tg_pca_prefix = os.path.join(TG_UKB_DIR, 'tg_pca')
-    x = pd.read_csv(tg_pca_prefix + '.sscore', sep='\t').rename(columns={'#IID': 'IID'}).set_index('IID').filter(like='_AVG')
-    y = pd.read_csv(tg_pca_prefix + '.tsv', sep='\t').set_index('IID').reindex(x.index)['ancestry']
-    _, y = np.unique(y, return_inverse=True)
-    # # CV is commented out because it's needed only to check performance
-    # SimpleTrainer(nclass=len(np.unique(y)), nfeat=x.shape[1], epochs=10000, lr=0.1).run_cv(x=x.values, y=y, K=10)
-    SimpleTrainer(nclass=len(np.unique(y)), nfeat=x.shape[1], epochs=10000, lr=0.1).train_and_save(x=x.values, y=y, out_fn=tg_pca_prefix + '.pkl')
-    logger.info(f'Done!')
 
